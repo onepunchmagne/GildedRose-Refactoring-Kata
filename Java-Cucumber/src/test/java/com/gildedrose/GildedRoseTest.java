51be562ea6305c6ffd3baaf534a6f23ca3772613
Cédric MAGNE <cmagne@ippon.fr>
2023-11-17 10:25:44 +0100

    extract contants

--- Java-Cucumber/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java-Cucumber/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,27 +1,26 @@
 package com.gildedrose;
 
-import junit.framework.TestCase;
 import org.junit.Test;
 
-import static org.assertj.core.api.Assertions.*;
-
 import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class GildedRoseTest {
 
     @Test
     public void shouldBehaveLikeGoldenMaster() {
-        GildedRose rose = new GildedRose(ItemsProvider.get());
+        GildedRose refactoring = new GildedRose(ItemsProvider.get());
         GildedRoseGolden golden = new GildedRoseGolden(ItemsProvider.get());
 
         for (int day = 0; day < 100; day++) {
-            rose.updateQuality();
+            refactoring.updateQuality();
             golden.updateQuality();
 
-            assertThat(rose.items)
+            assertThat(refactoring.items)
                     .extracting(item -> item.sellIn)
                     .containsExactly(Arrays.stream(golden.items).map(item -> item.sellIn).toArray(Integer[]::new));
-            assertThat(rose.items)
+            assertThat(refactoring.items)
                     .extracting(item -> item.quality)
                     .containsExactly(Arrays.stream(golden.items).map(item -> item.quality).toArray(Integer[]::new));
         }
